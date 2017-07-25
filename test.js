3ef0dfd839fa28ef47525074a7b7729120939df3
Jannis R <mail@jannisr.de>
2017-07-25 14:21:42 +0200

    tests :white_check_mark:

--- test.js
+++ test.js
@@ -4,5 +4,33 @@ const test = require('tape')
 
 const queryOverpass = require('.')
 
+const south = 52.45
+const west = 13.35
+const north = 52.5
+const east = 13.4
+const bbox = [south, west, north, east].join(',')
+const query = `\
+[out:json][timeout:25];
+node["railway"="subway_entrance"](${bbox});
+out body;`
+
 test('queryOverpass', (t) => {
+	queryOverpass(query)
+	.then((elements) => {
+		t.ok(Array.isArray(elements))
+		t.ok(elements.length > 0)
+		for (let el of elements) {
+			t.ok(el)
+			t.equal(typeof el.type, 'string')
+			t.equal(typeof el.id, 'number')
+			t.equal(typeof el.lat, 'number')
+			t.equal(typeof el.lon, 'number')
+			if (el.tags) {
+				t.notOk(Array.isArray(el.tags))
+				t.equal(typeof el.tags, 'object')
+			}
+		}
+		t.end()
+	})
+	.catch(t.ifError)
 })
