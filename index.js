456572b117aed3648d4bb209b53d254024216fc2
Jannis R <mail@jannisr.de>
2017-07-25 14:11:03 +0200

    implementation :clap:
    taken from https://github.com/derhuerst/vbb-osm-relations/blob/0f60566/build/query-overpass.js

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 'use strict'
 
-// todo
+const retry = require('p-retry')
+const {fetch} = require('fetch-ponyfill')({Promise: require('pinkie-promise')})
+const {stringify} = require('query-string')
+
+const endpoint = 'https://overpass-api.de/api/interpreter'
+
+const queryOverpass = (query) => {
+	return retry(() => {
+		return fetch(endpoint + '?' + stringify({data: query}), {
+			// todo: decide on this
+			// yields isomorphic code, but slower due to preflight request?
+			// mode: 'cors',
+			redirect: 'follow',
+			headers: {
+				accept: 'application/json',
+				'user-agent': 'http://github.com/derhuerst/vbb-osm-relations'
+			}
+		})
+		.then((res) => {
+			if (!res.ok) {
+				const err = new Error(res.statusText)
+				err.statusCode = res.status
+				throw err
+			}
+			return res.json()
+		})
+		.then((data) => {
+			if (!data || !Array.isArray(data.elements)) {
+				throw new Error('invalid response')
+			}
+			return data.elements
+		})
+	}, {minTimeout: 500})
+}
+
+module.exports = queryOverpass
